2b3171ca1f3141b89ce5638dfc359d5d53c90b74
Guillaume Duhamel <guillaume.duhamel@edukera.com>
2025-04-23 16:15:23 +0200

    fix

--- src/database/seeds/seed.module.ts
+++ src/database/seeds/seed.module.ts
@@ -2,9 +2,13 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AdminSeedService } from './admin.seed';
 import { User, UserSchema } from '../../modules/users/user.schema';
+import { DatabaseModule } from '../database.module';
+import { ConfigModule } from '../../config/config.module';
 
 @Module({
   imports: [
+    ConfigModule,
+    DatabaseModule,
     MongooseModule.forFeature([
       { name: User.name, schema: UserSchema },
     ]),
